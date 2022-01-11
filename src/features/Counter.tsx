f7e215c9e6d654cd6c772f6211391c8a9ac6dcb2
Alex Alan Nunes <alexalannunes@gmail.com>
2022-01-11 18:38:51 -0300

    implement async logic

--- src/features/Counter.tsx
+++ src/features/Counter.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { decrement, increment } from "./counterSlice";
+import {
+  decrement,
+  fetchByUsername,
+  increment,
+  incrementByAmount,
+} from "./counterSlice";
 
 const Counter: React.FC = () => {
   const counter = useAppSelector((state) => state.counter.value);
@@ -14,11 +19,24 @@ const Counter: React.FC = () => {
   const handleIncrement = () => {
     dispatch(increment());
   };
+
+  const handleIncrementByAmount = () => {
+    dispatch(incrementByAmount(10));
+  };
+
+  const handleFetchGithubUser = () => {
+    dispatch(fetchByUsername("alexalannunes"));
+  };
+
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={handleDecrement}>-</button>
       <button onClick={handleIncrement}>+</button>
+      <button onClick={handleIncrementByAmount}>+10</button>
+      <button onClick={handleFetchGithubUser}>
+        get alexalannunes' github id
+      </button>
     </div>
   );
 };
--- src/features/counterSlice.ts
+++ src/features/counterSlice.ts
@@ -1,5 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../store";
+import {
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+  Reducer,
+} from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
@@ -8,6 +12,21 @@ interface CounterState {
 const initialState: CounterState = {
   value: 0,
 };
+
+interface User {
+  login: string;
+  id: number;
+  avatar_url: string;
+}
+
+export const fetchByUsername = createAsyncThunk(
+  "counter/fetchSomeUsername",
+  async (username: string) => {
+    const req = await fetch(`https://api.github.com/users/${username}`);
+    const response = await req.json();
+    return response as User;
+  }
+);
 
 const counterSlice = createSlice({
   name: "counter",
@@ -19,9 +38,20 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      fetchByUsername.fulfilled,
+      (state, action: PayloadAction<User>) => {
+        state.value = action.payload.id;
+      }
+    );
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
-export default counterSlice.reducer;
+export default counterSlice.reducer as Reducer<CounterState>;
