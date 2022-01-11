c3ce8224d52c646dca732310f483de5cee762f3c
Alex Alan Nunes <alexalannunes@gmail.com>
2022-01-11 19:23:37 -0300

    add decrement and increment by amount test

--- src/features/Counter.test.ts
+++ src/features/Counter.test.ts
@@ -1,5 +1,18 @@
-import counterSlice, { increment } from "./counterSlice";
+import counterSlice, {
+  decrement,
+  fetchByUsername,
+  increment,
+  incrementByAmount,
+} from "./counterSlice";
 
 test("test counter increment", () => {
   expect(counterSlice({ value: 0 }, increment())).toEqual({ value: 1 });
+});
+test("test counter decrement", () => {
+  expect(counterSlice({ value: 0 }, decrement())).toEqual({ value: -1 });
+});
+test("test counter increment amount", () => {
+  expect(counterSlice({ value: 0 }, incrementByAmount(10))).toEqual({
+    value: 10,
+  });
 });
