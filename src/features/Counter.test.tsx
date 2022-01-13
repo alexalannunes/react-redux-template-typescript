c663b4de46547c8e2be2cfd7f447c048264ac654
Alex Alan Nunes <alexalannunes@gmail.com>
2022-01-13 13:10:15 -0300

    implement msw

--- src/features/Counter.test.tsx
+++ src/features/Counter.test.tsx
@@ -1,6 +1,23 @@
 import { fireEvent, render, screen } from "../test-utils";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { Counter } from "./Counter";
+export const handlers = [
+  rest.get("https://api.github.com/users/alexalannunes", (req, res, ctx) => {
+    return res(ctx.json({ id: "16847836" }), ctx.delay(150));
+  }),
+];
 
+const server = setupServer(...handlers);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
 test("click to increment, decrement and incrememnt amount", async () => {
   render(<Counter />);
 
